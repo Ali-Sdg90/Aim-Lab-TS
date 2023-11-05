9e031008287f2c9b11b474c302014dc8edbc97a5
Ali Sadeghi <ali909392@gmail.com>
2023-11-05 16:33:52 +0330

    Add Timer TS codes / Add basic styles / Learn how to tsc multiple ts files in one js file /

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,25 @@
 "use strict";
-console.log("Aloha!");
+let maxTimeCounter = 5;
+const timer = document.querySelector(".timer-section");
+const timerNumHelper = (timeNumber, fillerChar) => {
+    return String(Math.trunc(timeNumber)).padStart(2, fillerChar);
+};
+let TimeInterval = 0;
+const updateTimer = () => {
+    if (timer) {
+        const minValue = timerNumHelper(maxTimeCounter / 60, " ");
+        const secValue = timerNumHelper(maxTimeCounter % 60, "0");
+        timer.textContent = `${minValue}:${secValue}`;
+        if (!maxTimeCounter--) {
+            console.log("End of the timer");
+            clearInterval(TimeInterval);
+        }
+    }
+};
+updateTimer();
+if (timer) {
+    TimeInterval = setInterval(() => {
+        updateTimer();
+    }, 1000);
+}
 //# sourceMappingURL=index.js.map
--- TS/index.ts
+++ TS/index.ts
@@ -1,1 +1,33 @@
-console.log("Aloha!");
+// Timer.ts --------------------------------------------------
+
+let maxTimeCounter: number = 5;
+
+const timer: HTMLElement | null = document.querySelector(".timer-section");
+
+const timerNumHelper = (timeNumber: number, fillerChar: string): string => {
+    return String(Math.trunc(timeNumber)).padStart(2, fillerChar);
+};
+
+let TimeInterval: number = 0;
+
+const updateTimer = (): void => {
+    if (timer) {
+        const minValue: string = timerNumHelper(maxTimeCounter / 60, " ");
+        const secValue: string = timerNumHelper(maxTimeCounter % 60, "0");
+        timer.textContent = `${minValue}:${secValue}`;
+
+        if (!maxTimeCounter--) {
+            console.log("End of the timer");
+            clearInterval(TimeInterval);
+        }
+    }
+};
+updateTimer();
+
+if (timer) {
+    TimeInterval = setInterval(() => {
+        updateTimer();
+    }, 1000);
+}
+
+// -----------------------------------------------------------
