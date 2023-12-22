84387ac76cc75cdd7469906c8f1728c970fb963e
Ali Sadeghi <ali909392@gmail.com>
2023-12-22 16:08:15 +0330

    Add My CustomScaleCalc to project

---
+++ JS/customScaleCalc.js
@@ -0,0 +1,23 @@
+"use strict";
+const customScaleDiv = document.querySelector(".custom-scale-div");
+const scaleWarning = document.querySelector(".scale-warning");
+const pageWidth = customScaleDiv.scrollWidth;
+const pageHeight = customScaleDiv.scrollHeight;
+if (pageHeight > pageWidth) {
+    console.log(">>", pageHeight);
+    customScaleDiv.classList.add("custom-scale-calc");
+    let scale = 0;
+    scale = 1 - ((pageHeight / pageWidth) * 0.5) / (900 / 477);
+    if (pageHeight > 685) {
+        console.log("Mobile View");
+        customScaleDiv.style.transform = `scale(${scale}) rotate(90deg) translate(-50%, -53%)`;
+    }
+    else {
+        console.log("Small Mobile View");
+        customScaleDiv.style.height = "183vw";
+        customScaleDiv.style.transform = `scale(${scale}) rotate(90deg) translate(-50%, -50%)`;
+        scaleWarning.textContent =
+            "Your device is too small (height less than 685px), and my custom scale may not adjust itself properly to your device size. You may experience some inconsistencies in the UI.";
+    }
+}
+//# sourceMappingURL=customScaleCalc.js.map
